a483f3bef2cb003c784a02df67ba30f0f8399535
Dan Ellis <dan@electricwords.org>
2011-10-15 00:35:57 -0400

    Add variable name CLI arg

--- convert_rom.py
+++ convert_rom.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 import sys, textwrap
 
-rom = file(sys.argv[1], 'rb').read()
-chars = ', '.join('0x%02x' % ord(byte) for byte in rom)
-output = textwrap.wrap(chars, 76)
-file(sys.argv[2], 'w').write('\n'.join(output))
+if len(sys.argv) < 4:
+    print "Usage: %s <ROM file> <JS file> <variable name>" % sys.argv[0]
+    sys.exit(1)
+
+with file(sys.argv[1], 'rb') as ifile:
+    rom = ifile.read()
+    chars = ', '.join('0x%02x' % ord(byte) for byte in rom)
+    output = textwrap.wrap(chars, 76)
+
+with file(sys.argv[2], 'w') as ofile:
+    ofile.write('%s = new Uint8Array([\n' % sys.argv[3])
+    ofile.write('\n'.join(output))
+    ofile.write(']);\n')
